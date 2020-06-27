e910a6559e080d4e1f75217b529d9bbbd950876f
FaithCulas <faithculas@gmail.com>
2020-06-27 22:28:38 +0530

    frontend connected with backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import React, {Component} from 'react';
+import React, {Component,useEffect,useState} from 'react';
 import Activity from './components/Activity';
 import Authentication from './components/Authentication';
 import Localization from './components/Localization';
 import './App.css';
+import NavBar from './components/NavBar';
 
-class App extends Component{
-  render(){
+function App(){
+    const butstyle = {
+      marginTop: "5vh",
+      width: "40vh",
+      heigh: 100,
+      alighnContent: "center",}
+
+      const [activity,setActivity]=useState();
+      const [user,setUser]=useState();
+      const [location,setLocation]=useState();
+      
+
+      useEffect(()=>{
+        fetch('/get').then(res=>res.json().then(data=>{
+          console.log(data)
+          setActivity(data.activity);
+          setUser(data.user);
+          setLocation(data.location);
+        }))
+      },[])
+
+      
+
     return (
-      <div className="App">
-        <Activity/>
-        <Authentication/>
-        <Localization/>
+      <main>
+      <div>
+        <NavBar />
       </div>
+      <div className="container-fluid ">
+        <div className="row">
+          <div className="col" style={{ justifyContent: "center" }}>
+            <Activity />
+            {activity}
+          </div>
+          <div className="col">
+            <Authentication />
+            {user}
+            <Localization />
+            {location}
+            <div>
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={butstyle}
+              >
+                Add User
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+      
     );
   }
-}
  
 
 
